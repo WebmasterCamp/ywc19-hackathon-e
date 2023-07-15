0cffbb81720ec2d0b09e7547dd0b98be5d754b5d
Promchai Chooseang <promchai2sin@gmail.com>
2023-07-15 21:45:10 +0700

    First init

---
+++ src/lib/components/ItemBox.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let name = "", amount = 0, expired=new Date(), imageBox="";
+</script>
+
+<div class="h-[68px] pr-[109px] justify-start items-center gap-4 inline-flex">
+  <div class="w-6 h-6 p-0.5 justify-center items-center inline-flex">
+    <div class="w-5 h-5 relative bg-white rounded border border-zinc-900"></div>
+  </div>
+  <div class="w-[178px] h-[68px] relative">
+    <div class="left-[81px] top-0 absolute text-black text-base font-normal">
+      {name}
+    </div>
+    <div class="left-[81px] top-[53px] absolute text-black text-xs font-normal">
+      Expired in {expired.toDateString()} days
+    </div>
+    <div class="left-[81px] top-[29px] absolute text-neutral-600 text-xs font-normal">
+      Amount : {amount} unit
+    </div>
+    <div class="w-[68px] h-[68px] p-[22px] left-0 top-0 absolute bg-zinc-100 rounded-sm justify-center items-center inline-flex">
+      <img src={imageBox} alt="imagebox" />
+      <div class="w-6 h-6 relative flex-col justify-start items-start flex"></div>
+    </div>
+  </div>
+</div>
---
+++ src/routes/fridge/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import ItemBox from '$lib/components/ItemBox.svelte';
+    
+    export let data: PageData;
+</script>
+
+<p>Test</p>
+<div class="flex flex-col w-screen">
+    <div class="flex w-screen justify-between px-4">
+      <h1>My fridge</h1>
+      <div class="o">0</div>
+    </div>
+    <div class="flex space-x-3 px-4">
+      <a href="/">Meat</a>
+      <a>Vegetable</a>
+      <a>Dairy</a>
+    </div>
+    <div class="flex flex-col">
+      <div class="w-screen bg-gray-200 px-4">
+        <h2>Meat</h2>
+      </div>
+      <div class="flex flex-col space-y-4 px-4">
+        <ItemBox imageBox="/Image.svg" />
+        <ItemBox imageBox="/Image.svg" />
+        <ItemBox imageBox="/Image.svg" />
+      </div>
+    </div>
+    <div class="flex flex-col">
+      <div class="w-screen bg-gray-200 px-4">
+        <h2>Vegetable</h2>
+      </div>
+      <div class="flex flex-col space-y-4 px-4">
+        <ItemBox imageBox="/Image.svg" />
+        <ItemBox imageBox="/Image.svg" />
+        <ItemBox imageBox="/Image.svg" />
+        <ItemBox imageBox="/Image.svg" />
+      </div>
+    </div>
+    <div class="rounded-full w-20 h-20 bg-black">+</div>
+  </div>
