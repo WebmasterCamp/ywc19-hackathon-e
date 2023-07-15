591ffccb8f55a78d06094d83e101aa7349b069eb
Promchai Chooseang <promchai2sin@gmail.com>
2023-07-16 02:08:30 +0700

    Add the fridge add page

--- src/lib/components/ItemBoxEdit.svelte
+++ src/lib/components/ItemBoxEdit.svelte
@@ -4,16 +4,18 @@
   let expiredDate = (Date.now() - exp)/(1000 * 60 * 60 * 24);
 </script>
 
-<div class="w-80 h-16 pr-28 justify-start items-center gap-4 inline-flex">
-  <div class="w-6 h-6 p-0.5 justify-center items-center inline-flex">
-    <div class="w-5 h-5 relative bg-zinc-900 rounded flex-col justify-start items-start flex"></div>
+<div class="w-full justify-start items-center my-1 inline-flex">
+  <div class="mx-5 w-6 h-6 justify-center items-center inline-flex">
+    <checkbox class="w-5 h-5 relative bg-zinc-900 rounded flex-col justify-start items-start flex"></checkbox>
   </div>
-  <div class="w-44 h-16 relative">
-    <div class="w-16 h-16 p-5 left-0 top-0 absolute bg-zinc-100 rounded-sm justify-center items-center inline-flex">
-      <img src={image} alt="" class="w-6 h-6 relative flex-col justify-start items-start flex"/>
+  <div class="w-full h-16 flex">
+    <div class="w-16 h-16 p-5 my-auto bg-zinc-100 rounded-sm justify-center items-center inline-flex">
+      <img src={image} alt="" height="1.5rem" class="relative flex-col justify-start items-start flex"/>
     </div>
-    <div class="left-[81px] top-0 absolute text-black text-base font-normal">{name}</div>
-    <div class="left-[81px] top-[53px] absolute text-black text-xs font-normal">Expired in {expiredDate} days</div>
-    <div class="left-[81px] top-[29px] absolute text-neutral-600 text-xs font-normal">Amount : {amount} unit</div>
+    <div class="flex flex-col pl-4 ">
+      <h1 class="text-black text-base font-normal">{name}</h1>
+      <div class="text-black text-xs font-normal">Expired in {expiredDate} days</div>
+      <div class="text-neutral-600 text-xs font-normal">Amount : {amount} unit</div>
+    </div>
   </div>
 </div>
--- src/routes/fridge/add/+page.svelte
+++ src/routes/fridge/add/+page.svelte
@@ -6,24 +6,55 @@
   export let data;
 </script>
 
-<div class="flex flex-col w-screen">
-  <div class="flex w-screen justify-between px-4">
-    <form action="/fridge/add">
-      <div>
-        <label>
-          Search
-          <input name="search" type="text" />
-        </label>
+<div class="max-h-screen flex flex-col">
+  <div class="my-2">
+    <div class="flex items-center text-3xl">
+      <Icon icon="ic:round-arrow-back-ios" />
+      <h1 class="flex-grow text-center">Adjust Ingredients</h1>
+    </div>
+    <div class="mx-4">
+      <label
+        for="search"
+        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        >Search</label
+      >
+      <div class="relative">
+        <div
+          class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none"
+        >
+          <Icon class="text-black dark:text-white" icon="ic:baseline-search" />
+        </div>
+        <input
+          type="search"
+          id="search"
+          class="block w-full p-4 mt-4 pl-10 text-sm text-gray-900 border border-gray-800 rounded-full bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Search"
+          required
+        />
+        <button
+          type="submit"
+          class="h-8 w-8 absolute right-2.5 bottom-2.5 focus:ring-4 text-center"
+        >
+          <Icon icon="carbon:close-outline" class="text-black dark:text-white" />
+        </button>
       </div>
-      <button type="submit">search</button>
-    </form>
-  </div>
-  <div class="flex flex-col">
-    <div class="flex flex-col space-y-4 px-4">
-      {#each data.items as item}
-        <ItemBoxEdit image="/Image.svg" name={item.name} />
-      {/each}
     </div>
   </div>
-  <button class="bg-slate-800 text-slate-50">Confirm</button>
+  <div class="flex-glow">
+    <div class="w-full">
+      <div class="bg-gray-300 py-1 px-4">Meat</div>
+      <ItemBoxEdit />
+    </div>
+    <div class="w-full">
+      <div class="bg-gray-300 py-1 px-4">Vegetable</div>
+      <ItemBoxEdit />
+    </div>
+    <div class="w-full">
+      <div class="bg-gray-300 py-1 px-4">Daily</div>
+      <ItemBoxEdit />
+    </div>
+  </div>
+  <div class="">
+    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+  </div>
 </div>
