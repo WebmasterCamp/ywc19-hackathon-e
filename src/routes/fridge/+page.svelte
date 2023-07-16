33d464f8ac28732ab60aa5d711c868ca720b11a8
boomtnt2843 <boomphat@odds.team>
2023-07-16 10:44:41 +0700

    style

--- src/routes/fridge/+page.svelte
+++ src/routes/fridge/+page.svelte
@@ -13,11 +13,11 @@
     if ($ingredients.find((e: any) => e.name === obj.name)) {
       console.log("found");
       $ingredients = $ingredients.filter((e: any) => e.name != obj.name);
-      box?.classList.remove("!bg-gray-200");
+      box?.classList.remove("!bg-orange-200");
     } else {
       if ($ingredients.length < 3) {
         $ingredients = $ingredients.concat(obj);
-        box?.classList.add("!bg-gray-200");
+        box?.classList.add("!bg-orange-200");
       }
     }
   };
@@ -37,7 +37,7 @@
 
 <div class="flex flex-col w-screen pt-4">
   <div class="w-full border-b-2 px-4">
-    <img src="./image/mocking/logo.png" alt="logo">
+    <img src="./image/mocking/logo.png" alt="logo" />
   </div>
   <div class="flex w-screen justify-between px-4 items-baseline py-2">
     <h1 class="font-bold text-[20px]">My fridge</h1>
@@ -45,16 +45,15 @@
       <Icon class="text-black dark:text-white" icon="ic:outline-search" />
     </h1>
   </div>
-  <div class="flex space-x-3 px-4">
-    <button class="flex-1 bg-main">All</button>
-    <button class="flex-2 w-full">Meat</button>
-    <button class="flex-4 w-ful">Vegetable</button>
-    <button class="flex-2 w-full">Dairy</button>
+  <div class="flex space-x-3 p-4 ">
+    <button class="w-[10%] bg-main rounded-sm">All</button>
+    <button class="w-[30%] text-end pr-2 rounded-sm bg-drop">Meat</button>
+    <button class="w-[40%] text-end pr-2 rounded-sm bg-drop">Vegetable</button>
+    <button class="w-[30%] text-end pr-2 rounded-sm bg-drop">Dairy</button>
   </div>
   <div class="flex flex-col">
-    <div class="w-screen bg-gray-200 px-4 mb-2">
-      <h2>Meat</h2>
-    </div>
+    <div class="w-screen bg-drop-hard h-2" />
+    <h2 class="pl-4 py-2">Meat</h2>
     <div class="flex flex-wrap px-4 w-screen mb-2">
       {#each data.result.meat as item}
         <button on:click|stopPropagation={(e) => addToStore(item, e)}>
@@ -69,12 +68,13 @@
     </div>
   </div>
   <div class="flex flex-col">
-    <div class="w-screen bg-gray-200 px-4 mb-2">
-      <h2>Vegetable</h2>
-    </div>
+    <div class="w-screen bg-drop-hard h-2" />
+    <h2 class="pl-4 py-2">Vegetable</h2>
     <div class="flex flex-wrap px-4 w-screen mb-2">
       {#each data.result.veg as item}
-        <button on:click|stopPropagation={(e) => addToStore(item, this.Element)}>
+        <button
+          on:click|stopPropagation={(e) => addToStore(item, e)}
+        >
           <ItemBox
             name={item.name}
             exp={item.exp}
@@ -86,9 +86,8 @@
     </div>
   </div>
   <div class="flex flex-col">
-    <div class="w-screen bg-gray-200 px-4 mb-2">
-      <h2>Vegetable</h2>
-    </div>
+    <div class="w-screen bg-drop-hard h-2" />
+    <h2 class="pl-4 py-2">Vegetable</h2>
     <div class="flex flex-wrap px-4 w-screen mb-2">
       {#each data.result.dairy as item}
         <button on:click|stopPropagation={(e) => addToStore(item, e)}>
@@ -111,11 +110,13 @@
     <a class="text-white" href="/fridge/add">+</a>
   </div>
   <nav class="fixed bottom-0 w-screen border-t-2 border-" />
-  <button on:click={()=>{
-    new Notification("Hello", {
-      body: "Hello, world!",
-    });
-  }}>--noti--
+  <button
+    on:click={() => {
+      new Notification("Hello", {
+        body: "Hello, world!",
+      });
+    }}
+    >--noti--
   </button>
 </div>
 <Drawer
